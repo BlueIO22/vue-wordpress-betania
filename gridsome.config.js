294bc84e5b1321d817cd3b1e23f00e1b5525a576
Marius Sørenes <marius@fixit.no>
2020-11-02 19:05:21 +0100

    initial commit

---
+++ gridsome.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  siteName: 'Gridsome',
+  siteDescription: 'A WordPress starter for Gridsome',
+
+  templates: {
+    
+    WordPressPost: '/:year/:month/:day/:slug', // adds a route for the "post" post type (Optional)
+   },
+
+  plugins: [
+    {
+      use: '@gridsome/source-wordpress',
+      options: {
+        baseUrl: 'https://www.betaniastudentfellesskap.no/', // required
+        typeName: 'WordPress', // GraphQL schema name (Optional)
+        apiBase: 'wp-json',
+        typeName: 'WordPress',
+        perPage: 100,
+        concurrent: 10,
+        routes: {
+          post: '/:year/:month/:day/:slug',
+          post_tag: '/tag/:slug'
+        },
+        splitPostsIntoFragments: true, // default false
+        downloadRemoteImagesFromPosts: true, // default false
+        downloadRemoteFeaturedImages: true, // default false
+        downloadACFImages: true // default false
+      }
+    }
+  ]
+}
---
+++ src/components/Post.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h2 v-html="post.title" />
+    <div v-html="post.excerpt" />
+    <p v-html="post.content"></p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    post: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+<style scoped>
+p >>> img {
+  border: 1px solid black;
+}
+
+p >>> ul {
+  display: inline;
+  list-style: none;
+}
+
+p >>> ul li {
+  display:inline;
+}
+</style>
